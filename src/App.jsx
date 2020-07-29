4950556f6375f690c60c9049ec68d372a0a6a03b
Your Name <you@example.com>
2020-07-29 12:31:26 +0100

    getting data from data file

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import styles from "./App.module.scss";
 import Searchbar from "./components/Searchbar";
 import Cardlist from "./components/Cardlist"
+import films from "./data";
 
 class MovieDB extends Component {
   state = {  }
+
+  fetchMovies = () => {
+    alert("the button works")
+  }
+
+
   render() { 
+
+    const exampleFilm = films[0]
+
     return ( 
       <div>
         <p>The App</p>
         <Searchbar />
         <Cardlist />
+
+        <button onClick={this.fetchMovies}>Fetch info from Api</button>
+
+        <h2>Title: {exampleFilm.Title}</h2>
+        <p>Year: {exampleFilm.Year}</p>
+        <p>IMDB ID: {exampleFilm.imdbID}</p>
+        <p>Type: {exampleFilm.Type}</p>
+        <img src={exampleFilm.Poster} alt="poster"/>
+
       </div>
 
      );
@@ -18,3 +37,9 @@ class MovieDB extends Component {
 }
  
 export default MovieDB;
+
+// Here is your key: 2e1d8a0d
+
+// Please append it to all of your API requests,
+
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=2e1d8a0d
---
+++ src/data.js
@@ -0,0 +1,46 @@
+const films =  [
+  {
+      "Title": "Anne of Green Gables: The Continuing Story",
+      "Year": "2000",
+      "imdbID": "tt0158466",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjI2NzMxOTU2OV5BMl5BanBnXkFtZTcwMjU5ODA4Nw@@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Anne of Green Gables: The Animated Series",
+      "Year": "2000–",
+      "imdbID": "tt0283705",
+      "Type": "series",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNDNjNjQwOTgtZmIyNi00ZjA3LWI0N2EtYTMzYzM0NmNlZDhjXkEyXkFqcGdeQXVyMzI5MTAzMzI@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Franklin and the Green Knight: The Movie",
+      "Year": "2000",
+      "imdbID": "tt0290621",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTkyMTkxNjMzOF5BMl5BanBnXkFtZTYwNjE2NTc5._V1_SX300.jpg"
+  },
+  {
+      "Title": "Green Sails",
+      "Year": "2000",
+      "imdbID": "tt0213688",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNjg5Njc3ODYwNl5BMl5BanBnXkFtZTcwMzI5MDQ1Mw@@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Clockin' Green",
+      "Year": "2000",
+      "imdbID": "tt0218517",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYTY3MTYzOTctYzc1Mi00NTU3LTkxNTEtYTM5NDJiYWM1YjhlL2ltYWdlXkEyXkFqcGdeQXVyNTM3MDMyMDQ@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Green",
+      "Year": "2000",
+      "imdbID": "tt0788084",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMTkyMzgwNDk4Nl5BMl5BanBnXkFtZTcwNjI0MTQ1MQ@@._V1_SX300.jpg"
+  },
+];
+
+export default films;
